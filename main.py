3d0d003698c0b9f8ba21a2e76c3d6fbf1f971232
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-27 21:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
-app = FastAPI()
+# 👇 ADD THIS
+from fastapi.middleware.cors import CORSMiddleware
 
+# load model
 model = joblib.load("svm_sentiment_model.pkl")
 
+app = FastAPI()
+
+# 👇 ADD CORS HERE (IMPORTANT)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# request schema
 class TextInput(BaseModel):
     text: str
 
+# routes
 @app.get("/")
 def home():
-    return {"message": "AI API is running 🚀"}
+    return {"message": "API running 🚀"}
 
 @app.post("/predict")
 def predict(data: TextInput):
-    prediction = model.predict([data.text])
-    return {"prediction": prediction[0]}
+    prediction = model.predict([data.text])[0]
+    return {"prediction": prediction}
