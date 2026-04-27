7b41a1c71dbb17629f0750568cc5552690f2f87b
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-27 09:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
@@ -9,11 +10,11 @@ model = joblib.load("sentiment_model.pkl")
 class TextInput(BaseModel):
     text: str
 
-    @app.get("/")
-    def home():
-        return {"message": "AI API is running 🚀"}
+@app.get("/")
+def home():
+    return {"message": "AI API is running 🚀"}
 
-        @app.post("/predict")
-        def predict(data: TextInput):
-            prediction = model.predict([data.text])
-                return {"prediction": prediction[0]}
+@app.post("/predict")
+def predict(data: TextInput):
+    prediction = model.predict([data.text])
+    return {"prediction": prediction[0]}
