3b13cb88730a37a08c8ea3d44bcf09c92e5c1efb
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-27 09:04:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+model = joblib.load("sentiment_model.pkl")
+
+class TextInput(BaseModel):
+    text: str
+
+    @app.get("/")
+    def home():
+        return {"message": "AI API is running 🚀"}
+
+        @app.post("/predict")
+        def predict(data: TextInput):
+            prediction = model.predict([data.text])
+                return {"prediction": prediction[0]}
